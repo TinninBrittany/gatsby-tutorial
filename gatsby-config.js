e9784dfaa5b6452b7653af421084a925aad19223
Brittany Tinnin <bntinnin7@gmail.com>
2022-12-11 14:18:08 -0700

    add gatsby config and image component

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+ module.exports = {
+    siteMetadata: {
+      title: `gatsby-js-boilerplate`,
+      siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: ["gatsby-plugin-image",
+    "gatsby-plugin-sharp"],
+  }
+  
