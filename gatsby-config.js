bdf8448bd6d63f5e2de5cd2df94721383bacbbfb
Brittany Tinnin <bntinnin7@gmail.com>
2022-12-11 14:35:09 -0700

    add website favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,25 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
- module.exports = {
-    siteMetadata: {
-      title: `gatsby-js-boilerplate`,
-      siteUrl: `https://www.yourdomain.tld`,
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-js-boilerplate`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
     },
-    plugins: ["gatsby-plugin-image",
-    "gatsby-plugin-sharp"],
-  }
-  
+  ],
+};
